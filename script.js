d32674ce53bbad70655fe90805813bb568594da6
SachinSharma22 <s830sharma@gmail.com>
2025-02-28 22:09:51 +0530

    Learn about private and public variable and how to create object of a class

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 class Human{
-    
+    // Properties
+    age = 13; //public
+    #wt = 70;  //private :> When you want to make a variable private in JavaScript just use a # symbol before your variable
+    ht = 180;
+
+    // Behaviour
+
+    walking(){
+        console.log("I am Walking");
+        console.log("My current weight is ", this.#wt); // If you want to print a private variable use this keyword along with # symbol
+    }
+
+    running(){
+        console.log("I am running");
+    }
 }
+
+let obj = new Human();
+console.log(obj.age);
+console.log(obj.wt);  //It gives us undefined because wt is private and we can not access a private member out side of a scope
+obj.walking();
